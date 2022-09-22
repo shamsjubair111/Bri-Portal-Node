a6307b76bfa50f44f8538e869022df9a29033c8f
shams_jubair111 <2017-1-60-032@std.ewubd.edu>
2022-09-22 13:47:17 +0600

    1155

--- src/views/SMS/WithdrawTransaction/Index.js
+++ src/views/SMS/WithdrawTransaction/Index.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import { Card, CardBody } from 'reactstrap';
+import React, { useEffect, useState } from 'react';
+import { Card, CardBody, Table } from 'reactstrap';
+import Consultant from '../Dashboard/Pages/Consultant/Index';
+import Select from 'react-select';
 
 const Index = () => {
+
+    const [label,setLabel] = useState('Select Consultant');
+    const [consultant,setCounsultant] = useState([]);
+    const [value,setValue] = useState(0);
+
+    useEffect(()=>{
+
+    },[])
+
     return (
         <div>
             <Card>
                 <CardBody>
-                    
+                <Select
+                      value={{ label: label, value: value }}
+                      name="UniversityTypeId"
+                      id="UniversityTypeId"
+                    />
+
+                </CardBody>
+            </Card>
+
+            <Card>
+                <CardBody>
+                <Table id="table-to-xls" className="table-sm table-bordered">
+                <thead className="thead-uapp-bg">
+                  <tr style={{ textAlign: "center" }}>
+                    <th>SL/NO</th>
+                    <th>Transaction Date</th>
+                    <th>Agent Name</th>
+                    <th>Transaction Code</th>
+                    <th>Amount</th>
+                    <th>Reference/Invoice No.</th>
+                    <th>Payment Type</th>
+                    <th>Action</th>
+                   
+                 
+                  </tr>
+                </thead>
+                <tbody>
+                 
+                </tbody>
+              </Table>
                 </CardBody>
             </Card>
             
