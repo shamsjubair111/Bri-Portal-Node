6393767a98e724ee5d00c9630e9286232ee0f6fc
prantoBlueBayIt <prantobiswasbluebayit@gmail.com>
2022-10-09 18:34:53 +0600

    done

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
 
 // export const rootUrl = `http://192.168.0.115:45456/`
-export const rootUrl = `http://192.168.0.120:45457/`
+export const rootUrl = `http://192.168.0.115:45458/`
 
 // export const rootUrl = `http://api.uapp.uk/`
 
