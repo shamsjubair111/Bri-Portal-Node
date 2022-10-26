733c36ad371c5b299db9139421cb0432b010187c
prantoBlueBayIt <prantobiswasbluebayit@gmail.com>
2022-10-26 18:05:34 +0600

    embeded map input type changed to url

--- src/views/SMS/University/AddUniversityCampus.jsx
+++ src/views/SMS/University/AddUniversityCampus.jsx
@@ -754,12 +754,12 @@ const AddUniversityCampus = (props) => {
                       </Col>
                       <Col md="6">
                         <Input
-                          type="textarea"
+                          type="url"
                           rows="4"
                           name="EmbededMap"
                           id="EmbededMap"
                           defaultValue={universityCampusObject?.embededMap}
-                          placeholder="Embeded A Map"
+                          placeholder="https://example.com"
                         />
                         <span className="text-danger">Note: Please type the "src" link only from the embed map</span>
                         {/* <div className="form-control-position">
