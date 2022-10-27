e9833db90ff6dc92a85b17faa897dc70a5b2d8c8
prantoBlueBayIt <prantobiswasbluebayit@gmail.com>
2022-10-27 15:53:50 +0600

    done 4

--- build/static/js/296.3b0762d6.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpdesk_client=this.webpackJsonpdesk_client||[]).push([[296],{956:function(e,n,t){"use strict";t.r(n);var s=t(0),c=t.n(s);n.default=function(){return c.a.createElement("div",null)}}}]);
-//# sourceMappingURL=296.3b0762d6.chunk.js.map
