32410d451b8a02ff630af8fef27d24f513ec18c8
prantoBlueBayIt <prantobiswasbluebayit@gmail.com>
2022-12-15 15:13:49 +0600

    11

--- src/views/SMS/Applications/ApplicationDetails.js
+++ src/views/SMS/Applications/ApplicationDetails.js
@@ -14,11 +14,13 @@ import {
   Input,
   Button,
   CardFooter,
+  Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup,
 } from "reactstrap";
 
 import get from "../../../helpers/get";
 import { rootUrl } from "../../../constants/constants";
 import { useToasts } from "react-toast-notifications";
+import Select from "react-select";
 
 import { permissionList } from "../../../constants/AuthorizationConstant";
 import MessageHistoryCardApplicationDetailsPage from "./MessageHistoryCardApplicationDetailsPage";
@@ -28,6 +30,10 @@ import ApplicationInfo from "./ApplicationDetailsComponents/ApplicationInfo";
 import Loader from "../Search/Loader/Loader";
 import post from "../../../helpers/post";
 import Assessment from "./ApplicationDetailsComponents/Assessment";
+import ButtonForFunction from "../Components/ButtonForFunction";
+import put from "../../../helpers/put";
+import { userTypes } from "../../../constants/userTypeConstant";
+import ButtonLoader from "../Components/ButtonLoader";
 
 const ApplicationDetails = () => {
   const [activetab, setActivetab] = useState("1");
@@ -45,15 +51,38 @@ const ApplicationDetails = () => {
   const [etaDate, setEtaDate] = useState("");
   const [eatDeadLine, setEtaDeadLine] = useState("");
 
+  const [uniId, setUniId] = useState(0);
+  const [admissionManagerDD, setAdmissionManagerDD] = useState([]);
+  const [managerLabel, setManagerLabel] = useState("Select Admission Manager");
+  const [managerValue, setManagerValue] = useState(0);
+  const [managerError, setManagerError] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [managerId, setManagerId] = useState(0);
+
+  const [officerDD, setOfficerDD] = useState([]);
+  const [officerLabel, setOfficerLabel] = useState("Select Admission Officer");
+  const [officerValue, setOfficerValue] = useState(0);
+  const [officerError, setOfficerError] = useState(false);
+  const [modalOpen1, setModalOpen1] = useState(false);
+  const [officerId, setOfficerId] = useState(0);
+
+  const [progress1, setProgress1] = useState(false);
+  const [progress2, setProgress2] = useState(false);
+
   // const { addToast } = useToasts();
   const history = useHistory();
   const { id, stdId } = useParams();
   const location = useLocation();
+  const userType = localStorage.getItem("userType");
 
   useEffect(() => {
     get(`Application/Get/${id}`).then((res) => {
       setLoading(false);
       setApplicationInfo(res);
+      console.log("appInfo", res);
+      setManagerId(res?.admissionManager?.id);
+      setOfficerId(res?.admissionOfficer?.id);
+      setUniId(res?.university?.id);
       setElptDate(handleDate(res?.elpt?.elptDate));
       setEtaDate(handleDate(res?.elpt?.eta));
       setEtaDeadLine(handleDate(res?.elpt?.etaDeadline));
@@ -65,7 +94,6 @@ const ApplicationDetails = () => {
       setNotes(res);
 
     })
-
 
   }, [id, stdId, success]);
 
@@ -132,6 +160,141 @@ const ApplicationDetails = () => {
         });
       }
     })
+  }
+
+  const handleChange = (uId) => {
+    setModalOpen(true);
+    get(`AdmissionManagerDD/University/${uId}`)
+    .then(res => {
+      setAdmissionManagerDD(res);
+    })
+  }
+
+  const handleChange1 = (uId) => {
+    setModalOpen1(true);
+    get(`AdmissionOfficerDD/University/${uId}`)
+    .then(res => {
+      setOfficerDD(res);
+    })
+  }
+
+  const closeModal = () => {
+    setModalOpen(false);
+    setManagerLabel("Select Admission Manager");
+    setManagerValue(0);
+    
+  }
+
+  const closeModal1 = () => {
+    setModalOpen1(false);
+    setOfficerLabel("Select Admission Officer");
+    setOfficerValue(0);
+  }
+
+  const managerOptions = admissionManagerDD?.map((adm) => ({
+    label: adm?.name,
+    value: adm?.id,
+  }));
+
+  const officerOptions = officerDD?.map((ado) => ({
+    label: ado?.name,
+    value: ado?.id,
+  }));
+
+  const selectManager = (label, value) => {
+    setManagerLabel(label);
+    
+    if(value == managerId){
+      setManagerValue(null);
+    }
+    else{
+      setManagerValue(value);
+    }
+    setManagerError(false);
+  };
+
+  const selectOfficer = (label, value) => {
+    setOfficerLabel(label);
+    
+    if(value == officerId){
+      setOfficerValue(null);
+    }
+    else{
+      setOfficerValue(value);
+    }
+    setOfficerError(false);
+  };
+
+  const handleSubmitChange = (e) =>{
+    e.preventDefault();
+
+    if(managerValue === 0){
+      setManagerError(true);
+    }
+    else{
+      setProgress1(true);
+      put(`ApplicationAdmissionManager/ChangeAdmissionManager/${id}/${managerValue}`).then((res) => {
+            
+        // setButtonStatus(false);
+        // setProgress(false);
+        if (res?.status == 200 && res?.data?.isSuccess == true) {
+          addToast(res?.data?.message, {
+            appearance: "success",
+            autoDismiss: true,
+          });
+          
+          setModalOpen(false);
+          setSuccess(!success);
+          setProgress1(false);
+          setManagerLabel("Select Admission Manager");
+          setManagerValue(0);
+        }
+        else{
+          addToast(res?.data?.message, {
+            appearance: "error",
+            autoDismiss: true,
+          });
+          setProgress1(false);
+        }
+      });
+    }
+
+  }
+
+  const handleSubmitChange1 = (e) =>{
+    e.preventDefault();
+
+    if(officerValue === 0){
+      setOfficerError(true);
+    }
+    else{
+      setProgress2(true);
+      put(`ApplicationAdmissionOfficer/ChangeAdmissionOfficer/${id}/${officerValue}`).then((res) => {
+            
+        // setButtonStatus(false);
+        // setProgress(false);
+        if (res?.status == 200 && res?.data?.isSuccess == true) {
+          addToast(res?.data?.message, {
+            appearance: "success",
+            autoDismiss: true,
+          });
+          
+          setModalOpen1(false);
+          setSuccess(!success);
+          setProgress2(false);
+          setOfficerLabel("Select Admission Officer");
+          setOfficerValue(0);
+        }
+        else{
+          addToast(res?.data?.message, {
+            appearance: "error",
+            autoDismiss: true,
+          });
+          setProgress2(false);
+        }
+      });
+    }
+
   }
 
   // const handleUpdateTestScores = (data) => {
@@ -260,7 +423,7 @@ const ApplicationDetails = () => {
         <Col md="5" className="mb-5">
           <Card>
             <CardBody>
-              <div className="hedding-titel d-flex justify-content-between">
+              <div className="hedding-titel d-flex justify-content-between mb-3">
                 <div>
                   <h5>
                     {" "}
@@ -269,10 +432,107 @@ const ApplicationDetails = () => {
 
                   <div className="bg-h"></div>
                 </div>
-                {/* <div className="text-right edit-style  p-3">
-                 <span> <i className="fas fa-pencil-alt pencil-style"></i> </span>
-               </div> */}
+                <div className="text-right edit-style">
+                 {/* <span onClick={() => setModalOpen(true)}> <i className="fas fa-pencil-alt pencil-style"></i> </span> */}
+
+                 {
+                   userType == userTypes?.SystemAdmin || userType == userTypes?.Admin || userType == userTypes?.ProviderAdmin ?
+                   <ButtonForFunction
+                     func={()=> handleChange(uniId)}
+                     className={"btn btn-uapp-add "}
+                    //  icon={<i className="fas fa-plus"></i>}
+                     name={"Change"}
+                     permission={6}
+                   />
+                   :
+                   null
+                 }
+
+               </div>
               </div>
+
+              <Modal isOpen={modalOpen} toggle={closeModal} className="uapp-modal">
+                <ModalHeader>Change Admission Manager</ModalHeader>
+                <ModalBody>
+                <Form onSubmit={handleSubmitChange}>
+  
+                  {/* {
+                    programLvlId != undefined ?
+                    <input
+                      type='hidden'
+                      name='id'
+                      id='id'
+                      value={programLvlId}
+                    />
+                        :
+                        null
+                  } */}
+         
+                      <FormGroup row className="has-icon-left position-relative">
+                        <Col md="4">
+                          <span>Admission Manager <span className="text-danger">*</span>{" "}</span>
+                        </Col>
+                        <Col md="8">
+                        <Select
+                          options={managerOptions}
+                          value={{ label: managerLabel, value: managerValue }}
+                          onChange={(opt) => selectManager(opt.label, opt.value)}
+                          name="id"
+                          id="id"
+                          // isDisabled={univerTypeId !== undefined ? true : false}
+                      />
+
+                      {managerError ? (
+                        <span className="text-danger">
+                          Admission Manager is required
+                        </span>
+                      ) : null}
+
+                      {
+                        managerValue == null ?
+                        <span className="text-danger">
+                          Can not change to same admission manager
+                        </span>
+                        :
+                        null
+                      }
+
+                    </Col>
+                  </FormGroup>
+                      
+                  <FormGroup className="has-icon-left position-relative" style={{ display: 'flex', justifyContent: 'space-between' }}>
+                      
+                    <Button color="danger" className="mr-1 mt-3" onClick={closeModal}>Close</Button>
+                      
+                    {/* {
+                    localStorage.getItem("ProgramId") ?
+                      <Button color="primary" onClick={handleUpdateSubmit}  className="mr-1 mt-3">Update</Button> : */}
+                      {/* <Button.Ripple
+                        color="primary"
+                        type="submit"
+                        className="mr-1 mt-3"
+                      >
+                        Submit
+                      </Button.Ripple> */}
+
+                      <ButtonForFunction
+                        color={"primary"}
+                        type={"submit"}
+                        className={"mr-1 mt-3"}
+                        name={progress1 ? <ButtonLoader/> : "Submit"}
+                        // name={"Submit"}
+                        permission={6}
+                        disable={managerValue == null ? true : false}
+                      />
+
+                  {/* } */}
+                    
+                  </FormGroup>
+                    
+                </Form>
+                </ModalBody>
+                    
+                </Modal>
 
               <div className="d-flex justify-content-between">
                 <div className="my-auto">
@@ -310,6 +570,186 @@ const ApplicationDetails = () => {
                   </div>
                 </div>
               </div>
+            </CardBody>
+          </Card>
+
+          <Card>
+            <CardBody>
+              <div className="hedding-titel d-flex justify-content-between mb-3">
+                <div>
+                  <h5>
+                    {" "}
+                    <b>Admission Officer</b>{" "}
+                  </h5>
+
+                  <div className="bg-h"></div>
+                </div>
+                <div className="text-right edit-style">
+                 {/* <span onClick={() => setModalOpen(true)}> <i className="fas fa-pencil-alt pencil-style"></i> </span> */}
+
+                 {
+                  applicationInfo?.admissionOfficer !== null ?
+                  <>
+                    {
+                      userType == userTypes?.SystemAdmin || userType == userTypes?.Admin || userType == userTypes?.ProviderAdmin ?
+                      <ButtonForFunction
+                        func={()=> handleChange1(uniId)}
+                        className={"btn btn-uapp-add "}
+                       //  icon={<i className="fas fa-plus"></i>}
+                        name={"Change"}
+                        permission={6}
+                      />
+                      :
+                      null
+                    }
+                  </>
+                  :
+                  null
+                 }
+
+               </div>
+              </div>
+
+              <Modal isOpen={modalOpen1} toggle={closeModal1} className="uapp-modal">
+                <ModalHeader>Change Admission Officer</ModalHeader>
+                <ModalBody>
+                <Form onSubmit={handleSubmitChange1}>
+  
+                  {/* {
+                    programLvlId != undefined ?
+                    <input
+                      type='hidden'
+                      name='id'
+                      id='id'
+                      value={programLvlId}
+                    />
+                        :
+                        null
+                  } */}
+         
+                      <FormGroup row className="has-icon-left position-relative">
+                        <Col md="4">
+                          <span>Admission Officer <span className="text-danger">*</span>{" "}</span>
+                        </Col>
+                        <Col md="8">
+                        <Select
+                          options={officerOptions}
+                          value={{ label: officerLabel, value: officerValue }}
+                          onChange={(opt) => selectOfficer(opt.label, opt.value)}
+                          name="id"
+                          id="id"
+                          // isDisabled={univerTypeId !== undefined ? true : false}
+                      />
+
+                      {officerError ? (
+                        <span className="text-danger">
+                          Admission Officer is required
+                        </span>
+                      ) : null}
+
+                      {officerValue == null ? (
+                        <span className="text-danger">
+                          Can not change to same admission officer
+                        </span>
+                      ) : null}
+
+                    </Col>
+                  </FormGroup>
+                      
+                  <FormGroup className="has-icon-left position-relative" style={{ display: 'flex', justifyContent: 'space-between' }}>
+                      
+                    <Button color="danger" className="mr-1 mt-3" onClick={closeModal1}>Close</Button>
+                      
+                    {/* {
+                    localStorage.getItem("ProgramId") ?
+                      <Button color="primary" onClick={handleUpdateSubmit}  className="mr-1 mt-3">Update</Button> : */}
+                      {/* <Button.Ripple
+                        color="primary"
+                        type="submit"
+                        className="mr-1 mt-3"
+                      >
+                        Submit
+                      </Button.Ripple> */}
+
+                      <ButtonForFunction
+                        color={"primary"}
+                        type={"submit"}
+                        className={"mr-1 mt-3"}
+                        name={progress2 ? <ButtonLoader/> : "Submit"}
+                        // name={"Submit"}
+                        permission={6}
+                        disable={officerValue == null ? true : false}
+                      />
+
+                  {/* } */}
+                    
+                  </FormGroup>
+                    
+                </Form>
+                </ModalBody>
+                    
+                </Modal>
+
+                {
+                  applicationInfo?.admissionOfficer !== null ?
+                  <div className="d-flex justify-content-between">
+                    <div className="my-auto">
+                      <span>
+                        <b>Name:</b> {applicationInfo?.admissionOfficer?.firstName}{" "}
+                        {applicationInfo?.admissionOfficer?.lastName}
+                      </span>{" "}
+                      <br />
+                      <span>
+                        <b>Email:</b> {applicationInfo?.admissionOfficer?.email}
+                      </span>
+                      <br />
+                      <span>
+                        <b>Phone:</b>{" "}
+                        {applicationInfo?.admissionOfficer?.phoneNumber}
+                      </span>
+                    </div>
+                    <div>
+                      <div className="uapp-circle-image margin-top-minus">
+                        {/* {applicationInfo?.admissionManager?.admissionManagerMedia?.fileUrl ==
+                      null ? (
+                        <img src={profileImage} alt="provider_img" />
+                      ) : ( */}
+
+                        <img
+                          src={
+                            rootUrl +
+                            applicationInfo?.admissionOfficer?.admissionOfficerMedia
+                              ?.thumbnailUrl
+                          }
+                          alt="admission_officer_img"
+                        />
+
+                        {/* )} */}
+                      </div>
+                    </div>
+                  </div>
+                  :
+                  <div>
+                    <div className="text-center mb-2">
+                      <b>Admission Officer is not assigned here.</b>
+                    </div>
+                    <center>
+                    {
+                      userType == userTypes?.SystemAdmin || userType == userTypes?.Admin || userType == userTypes?.ProviderAdmin ?
+                      <ButtonForFunction
+                        func={()=> handleChange1(uniId)}
+                        className={"btn btn-uapp-add "}
+                       //  icon={<i className="fas fa-plus"></i>}
+                        name={"Assign"}
+                        permission={6}
+                      />
+                      :
+                      null
+                    }
+                    </center>
+                  </div>
+                }
+
             </CardBody>
           </Card>
 
