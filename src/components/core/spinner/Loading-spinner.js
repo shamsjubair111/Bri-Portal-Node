42fe9ecd5bb0d9c2fd8b85e6b902492fc7e7d3bb
Momenuzzaman <mdeamon4@gmail.com>
2024-08-01 17:26:59 +0600

    loading img change

--- src/components/core/spinner/Loading-spinner.js
+++ src/components/core/spinner/Loading-spinner.js
@@ -1,20 +1,22 @@
-import React from "react"
-import "../../../assets/scss/components/app-loader.scss"
-import loader from '../../../assets/img/load.gif';
+import React from "react";
+import "../../../assets/scss/components/app-loader.scss";
+import loader from "../../../assets/img/CCL_Logo.png";
 class ComponentSpinner extends React.Component {
   render() {
     return (
-      <div className="text-center" style={{backgroundColor: 'fff',height: '100vh'}}>
+      <div
+        className="text-center"
+        style={{ backgroundColor: "fff", height: "100vh" }}
+      >
         {/* <div className="loading component-loader">
           <div className="effect-1 effects"></div>
           <div className="effect-2 effects"></div>
           <div className="effect-3 effects"></div>
         </div> */}
-        <img src={loader}  className="img-fluid" alt="uapp_loader_image" />
-
+        <img src={loader} className="img-fluid" alt="uapp_loader_image" />
       </div>
-    )
+    );
   }
 }
 
-export default ComponentSpinner
+export default ComponentSpinner;
